28cd331d80b01c3b06e3e2b32323e94031b3b4f3
Leo Sena <senaleonardo170@gmail.com>
2026-01-15 18:38:06 -0300

    Beecrowd 1000 - Hello World

---
+++ src/1000-hello-world/main.js
@@ -0,0 +1,3 @@
+console.log("Hello World!")
+// Console.log() Mesmo não estando atribuido a algo dentro do codigo, 
+// o console.log ele exibe qualquer coisa que estiver entre os seus (Qualquer coisa)git
